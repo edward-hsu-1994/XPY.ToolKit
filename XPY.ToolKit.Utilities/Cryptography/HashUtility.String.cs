3816a6d4b5c59caea77c6552164fa756f7527855
XuPeiYao <st89382000@gmail.com>
2019-08-22 00:13:54 +0800

    feat: 加入常見雜湊公用方法
    支援 String, Stream, byte[] 的雜湊值計算

---
+++ XPY.ToolKit.Utilities/Cryptography/HashUtility.String.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace XPY.ToolKit.Utilities.Cryptography {
+    /// <summary>
+    /// 雜湊計算公用方法
+    /// </summary>
+    public static partial class HashUtility {
+        /// <summary>
+        /// 將字串使用指定的雜湊演算法轉換為雜湊
+        /// </summary>
+        /// <typeparam name="Algorithm">雜湊演算法型別</typeparam>
+        /// <param name="str">值</param>
+        /// <returns>雜湊Binary</returns>
+        public static byte[] ToHash<Algorithm>(string str) where Algorithm : HashAlgorithm {
+            using (var hash = typeof(Algorithm)
+                .GetMethod("Create", new Type[] { })
+                .Invoke(null, null) as HashAlgorithm) {
+                return hash.ComputeHash(Encoding.UTF8.GetBytes(str));
+            }
+        }
+
+        /// <summary>
+        /// 將字串使用指定的雜湊演算法轉換為雜湊後在轉換為16進位字串表示
+        /// </summary>
+        /// <typeparam name="Algorithm">雜湊演算法型別</typeparam>
+        /// <param name="str">值</param>
+        /// <param name="upper">是否轉換為大寫</param>
+        /// <returns>雜湊字串</returns>
+        public static string ToHashString<Algorithm>(string str, bool upper = true) where Algorithm : HashAlgorithm {
+            return string.Join("", ToHash<Algorithm>(str).Select(x => x.ToString(upper ? "X2" : "x2")));
+        }
+    }
+}
