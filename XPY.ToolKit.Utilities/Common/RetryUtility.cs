b924e6d06219f90ec45a98114f1fb610f0e321c5
XuPeiYao <st89382000@gmail.com>
2019-08-25 13:24:00 +0800

    feat: 加入重試操作常用方法

---
+++ XPY.ToolKit.Utilities/Common/RetryUtility.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XPY.ToolKit.Utilities.Common {
+    /// <summary>
+    /// 重試操作方法
+    /// </summary>
+    public static class RetryUtility {
+        /// <summary>
+        /// 重試操作
+        /// </summary>
+        /// <typeparam name="T">回應類型</typeparam>
+        /// <param name="retryTime">重試次數</param>
+        /// <param name="func">操作方法</param>
+        /// <returns>操作結果</returns>
+        public static T Retry<T>(int retryTime, Func<T> func) {
+            Exception exception = null;
+            for (int i = 0; i < retryTime; i++) {
+                try {
+                    return func();
+                } catch (Exception e) {
+                    exception = e;
+                }
+            }
+            throw exception;
+        }
+
+        /// <summary>
+        /// 重試操作
+        /// </summary>
+        /// <param name="retryTime">重試次數</param>
+        /// <param name="action">操作方法</param>
+        public static void Retry(int retryTime, Action action) {
+            Retry(retryTime, () => {
+                action();
+                return 0;
+            });
+        }
+    }
+}
