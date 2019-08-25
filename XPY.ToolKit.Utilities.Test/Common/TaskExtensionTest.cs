5b30b493d2b45a5fded85190ce966022397756b7
XuPeiYao <st89382000@gmail.com>
2019-08-25 15:02:07 +0800

    test: 加入Task擴充方法單元測試

---
+++ XPY.ToolKit.Utilities.Test/Common/TaskExtensionTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XPY.ToolKit.Utilities.Common.Test {
+    public class TaskExtensionTest {
+        public async Task TestAsyncMethodAction() {
+            await Task.Run(() => {
+                return 0;
+            });
+        }
+
+        public async Task<int> TestAsyncMethodFunc() {
+            return await Task.Run(() => {
+                return 0;
+            });
+        }
+
+        [Fact(DisplayName = "非同步函數轉換同步方法測試")]
+        public void ToSyncAction() {
+            TestAsyncMethodAction().ToSync();
+        }
+
+        [Fact(DisplayName = "非同步方法轉換同步函數測試")]
+        public void ToSyncFunc() {
+            var result = TestAsyncMethodFunc().ToSync();
+            Assert.Equal(0, result);
+        }
+    }
+}
