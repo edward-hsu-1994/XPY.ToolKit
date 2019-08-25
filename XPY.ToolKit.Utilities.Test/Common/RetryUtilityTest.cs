62261fecb9fab46e09a892637c0d465e14538b6f
XuPeiYao <st89382000@gmail.com>
2019-08-25 13:24:10 +0800

    test: 加入重試操作常用方法單元測試

---
+++ XPY.ToolKit.Utilities.Test/Common/RetryUtilityTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XPY.ToolKit.Utilities.Common {
+    /// <summary>
+    /// 重試操作方法
+    /// </summary>
+    public static class RetryUtilityTest {
+        [Fact(DisplayName = "重試函數操作測試")]
+        public static void RetryFuncTest() {
+            int retry = 0;
+            RetryUtility.Retry(3, () => {
+                retry++;
+            });
+            Assert.Equal(1, retry);
+
+            retry = 0;
+            Assert.Throws<Exception>(() => {
+                RetryUtility.Retry(3, () => {
+                    retry++;
+                    throw new Exception();
+                });
+            });
+            Assert.Equal(3, retry);
+        }
+
+        [Fact(DisplayName = "重試方法操作測試")]
+        public static void RetryTest() {
+            int retry = 0;
+            bool result = RetryUtility.Retry(3, () => {
+                retry++;
+
+                return true;
+            });
+            Assert.Equal(1, retry);
+            Assert.True(result);
+
+            retry = 0;
+            Assert.Throws<Exception>(() => {
+                result = RetryUtility.Retry(3, () => {
+                    retry++;
+                    throw new Exception();
+
+                    return true;
+                });
+            });
+            Assert.Equal(3, retry);
+        }
+    }
+}
