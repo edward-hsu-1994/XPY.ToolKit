5772754c7dadaf318ef7b23ae1d458992e56041b
XuPeiYao <st89382000@gmail.com>
2019-08-25 14:02:59 +0800

    feat: 加入讀寫鎖區塊

---
+++ XPY.ToolKit.Utilities/Common/ReaderWriterLockScope.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace XPY.ToolKit.Utilities.Common {
+    /// <summary>
+    /// 讀寫鎖區塊模式
+    /// </summary>
+    public enum ReaderWriterLockMode {
+        Write,
+        Read,
+        UpgradeableRead
+    }
+
+    /// <summary>
+    /// 讀寫鎖區塊
+    /// </summary>
+    public class ReaderWriterLockScope : IDisposable {
+        ReaderWriterLockMode mode;
+        ReaderWriterLockSlim locker;
+        public ReaderWriterLockScope(ReaderWriterLockSlim locker, ReaderWriterLockMode mode) {
+            this.locker = locker;
+            this.mode = mode;
+            switch (mode) {
+                case ReaderWriterLockMode.Read:
+                    locker.EnterReadLock();
+                    break;
+                case ReaderWriterLockMode.Write:
+                    locker.EnterWriteLock();
+                    break;
+                case ReaderWriterLockMode.UpgradeableRead:
+                    locker.EnterUpgradeableReadLock();
+                    break;
+            }
+        }
+
+        public void Dispose() {
+            switch (mode) {
+                case ReaderWriterLockMode.Read:
+                    locker.ExitReadLock();
+                    break;
+                case ReaderWriterLockMode.Write:
+                    locker.ExitWriteLock();
+                    break;
+                case ReaderWriterLockMode.UpgradeableRead:
+                    locker.ExitUpgradeableReadLock();
+                    break;
+            }
+        }
+    }
+}
