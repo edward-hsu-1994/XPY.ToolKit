d3152ba4b428f3610f9f3f716e9d36a954f5dc09
XuPeiYao <st89382000@gmail.com>
2019-09-01 22:57:59 +0800

    feat: 加入IQueryable的處理轉換擴充方法

---
+++ XPY.ToolKit.Linq/Processing/ProcessedEnumerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XPY.ToolKit.Linq.Processing {
+    internal class ProcessedEnumerator<TSource, TResult> : IEnumerator<TResult> {
+        public IEnumerator<TSource> Source { get; internal set; }
+
+        public Func<TSource, TResult> Process { get; internal set; }
+
+        public TResult Current => Process(Source.Current);
+
+        object IEnumerator.Current => this.Current;
+
+        public ProcessedEnumerator(IEnumerator<TSource> source, Func<TSource, TResult> process) {
+            Source = source;
+            Process = process;
+        }
+
+        public virtual void Dispose() {
+            Source.Dispose();
+        }
+
+        public virtual bool MoveNext() {
+            return Source.MoveNext();
+        }
+
+        public virtual void Reset() {
+            Source.Reset();
+        }
+    }
+}
---
+++ XPY.ToolKit.Linq/Processing/ProcessedQueryable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace XPY.ToolKit.Linq.Processing {
+    internal class ProcessedQueryable<TSource, TResult> : IQueryable<TResult> {
+        public IQueryable<TSource> Source { get; internal set; }
+        public Func<TSource, TResult> Process { get; internal set; }
+
+        public Type ElementType => Source.ElementType;
+
+        public Expression Expression => Source.Expression;
+
+        public IQueryProvider Provider => Source.Provider;
+
+        public IEnumerator<TResult> GetEnumerator() {
+            return new ProcessedEnumerator<TSource, TResult>(
+                Source.GetEnumerator(),
+                Process
+            );
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() {
+            return this.GetEnumerator();
+        }
+    }
+}
