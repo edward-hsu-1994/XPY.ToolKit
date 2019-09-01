2260ce94cc45cfefa8a41fdd8d287257a84a844d
XuPeiYao <st89382000@gmail.com>
2019-09-01 22:37:28 +0800

    refactor: 移除與MoreLinq套件重複的方法並加入了分頁方法

---
+++ XPY.ToolKit.Linq/IQueryableExtension.Filter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace XPY.ToolKit.Linq {
+    public static partial class IQueryableExtension {
+        /// <summary>
+        /// 針對指定屬性取得值相等的成員
+        /// </summary>
+        /// <typeparam name="TSource">列舉元素類型</typeparam>
+        /// <typeparam name="TProperty">條件屬性類型</typeparam>
+        /// <param name="source">列舉來源</param>
+        /// <param name="selector">查詢屬性</param>
+        /// <param name="value">值，如為空則表示不篩選</param>
+        /// <returns>查詢結果</returns>
+        public static IQueryable<TSource> Filter<TSource, TProperty>(
+            this IQueryable<TSource> source,
+            Expression<Func<TSource, TProperty>> selector,
+            Nullable<TProperty> value)
+            where TProperty : struct {
+            var result = source;
+
+            var p = Expression.Parameter(typeof(TSource), "x");
+
+            if (value.HasValue) {
+                return result.Where(
+                        Expression.Lambda<Func<TSource, bool>>(
+                            Expression.Equal(
+                                Expression.Constant(value, typeof(Nullable<TProperty>)),
+                                Expression.Constant(null, typeof(Nullable<TProperty>))
+                            ), p)
+                      );
+            } else {
+                return result;
+            }
+        }
+    }
+}
---
+++ XPY.ToolKit.Linq/Paging/CommonPagingResult.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XPY.ToolKit.Linq.Paging {
+    /// <summary>
+    /// 通用分頁結果
+    /// </summary>
+    /// <typeparam name="TSource">列舉成員類型</typeparam>
+    public class CommonPagingResult<TSource> : IPagingResult<TSource> {
+        /// <summary>
+        /// 分頁資料來源
+        /// </summary>
+        public virtual IEnumerable<TSource> Source { get; private set; }
+
+        /// <summary>
+        /// 分頁結果
+        /// </summary>
+        public virtual IEnumerable<TSource> Result {
+            get {
+                IEnumerable<TSource> result = null;
+                if (Offset == -1) {
+                    result = Source.Skip(Offset);
+                } else {
+                    result = Source.Skip(Offset).Take(Limit).ToArray();
+                }
+
+                return result;
+            }
+        }
+
+        /// <summary>
+        /// 起始索引
+        /// </summary>
+        public virtual int Offset { get; private set; }
+
+        /// <summary>
+        /// 取得筆數
+        /// </summary>
+        public virtual int Limit { get; private set; }
+
+        /// <summary>
+        /// 資料總數
+        /// </summary>
+        public virtual int TotalCount => Source.Count();
+
+        /// <summary>
+        /// 目前所在分頁索引
+        /// </summary>
+        public virtual int CurrentPageIndex {
+            get {
+                if (Offset == -1) return 0;
+                return (int)Math.Floor(Offset / (double)Limit);
+            }
+        }
+
+        /// <summary>
+        /// 總頁數
+        /// </summary>
+        public virtual int TotalPageCount {
+            get {
+                if (Offset == -1) return 1;
+                return (int)Math.Ceiling(TotalCount / (double)Limit);
+            }
+        }
+
+        /// <summary>
+        /// 是否有上一頁
+        /// </summary>
+        public virtual bool HasPreviousPage => CurrentPageIndex > 0;
+
+        /// <summary>
+        /// 是否有下一頁
+        /// </summary>
+        public virtual bool HasNextPage => CurrentPageIndex < TotalPageCount - 1;
+
+        /// <summary>
+        /// 通用分頁結果預設建構子
+        /// </summary>
+        [Obsolete("此建構子僅供Proxy使用", true)]
+        public CommonPagingResult() { }
+
+        /// <summary>
+        /// 通用分頁結果建構子
+        /// </summary>
+        /// <param name="source">分頁資料來源</param>
+        /// <param name="offset">起始索引</param>
+        /// <param name="limit">取得筆數，如果為-1則表示取得所有資訊不分頁</param>
+        public CommonPagingResult(IEnumerable<TSource> source, int offset, int limit) {
+            this.Source = source;
+            this.Offset = offset;
+            this.Limit = limit;
+        }
+
+        /// <summary>
+        /// 移動目前所在分頁索引至指定索引
+        /// </summary>
+        /// <param name="pageIndex">頁數索引</param>
+        /// <returns></returns>
+        public virtual bool MoveToPage(int pageIndex) {
+            if (Offset == -1 && pageIndex != 0) {
+                return false;
+            }
+
+            var newOffset = Offset * pageIndex;
+
+            if (newOffset < 0 || newOffset >= TotalCount) return false;
+
+            Offset = newOffset;
+
+            return true;
+        }
+
+        /// <summary>
+        /// 取得指定分頁索引的分頁物件，如無下個分頁則返回<see cref="null"/>
+        /// </summary>
+        /// <param name="pageIndex">頁數索引</param>
+        /// <returns>分頁物件</returns>
+        public virtual CommonPagingResult<TSource> GetMoveToPage(int pageIndex) {
+            if (Offset == -1 && pageIndex != 0) {
+                pageIndex = 0;
+            }
+
+            var newOffset = Offset * pageIndex;
+
+            if (newOffset < 0 || newOffset >= TotalCount) return null;
+
+            return new CommonPagingResult<TSource>(Source, newOffset, Limit);
+        }
+
+        /// <summary>
+        /// 前後頁移動目前所在分頁索引
+        /// </summary>
+        /// <param name="deltaPageCount">分頁索引變動量</param>
+        /// <returns>是否移動成功</returns>
+        public virtual bool MovePage(int deltaPageCount) {
+            if (Limit == -1 && deltaPageCount != 0) {
+                return false;
+            }
+
+            var newOffset = Offset + Limit * deltaPageCount;
+
+            if (newOffset < 0 || newOffset >= TotalCount) return false;
+
+            Offset = newOffset;
+
+            return true;
+        }
+
+        /// <summary>
+        /// 取得前後頁移動目前所在分頁索引後的新的分頁物件，如無下個分頁則返回<see cref="null"/>
+        /// </summary>
+        /// <param name="deltaPageCount">分頁索引變動量</param>
+        /// <returns>分頁物件</returns>
+        public virtual CommonPagingResult<TSource> GetMovePage(int deltaPageCount) {
+            if (Offset == -1 && deltaPageCount != 0) {
+                return null;
+            }
+
+            var newOffset = Offset + Limit * deltaPageCount;
+
+            if (newOffset < 0 || newOffset >= TotalCount) return null;
+
+            return new CommonPagingResult<TSource>(Source, newOffset, Limit);
+        }
+
+        /// <summary>
+        /// 重設目前分頁索引為0
+        /// </summary>
+        public virtual void Reset() {
+            MoveToPage(0);
+        }
+    }
+}
