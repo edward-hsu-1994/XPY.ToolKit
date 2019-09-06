8bf3cd2832b232c133e4af4744b71c0463a56c4b
XuPeiYao <st89382000@gmail.com>
2019-09-07 02:01:37 +0800

    feat: 加入MVC的FormData JSON模型綁定功能

---
+++ XPY.ToolKit.AspNetCore.Mvc/FormJsonModelBinderProvider.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XPY.ToolKit.AspNetCore.Mvc {
+    /// <summary>
+    /// FormData JSON模型綁定器提供者
+    /// </summary>
+    public class FormJsonModelBinderProvider : IModelBinderProvider {
+        public IModelBinder GetBinder(ModelBinderProviderContext context) {
+            if (context == null) throw new ArgumentNullException(nameof(context));
+
+            if (context.BindingInfo?.BindingSource?.DisplayName == "FormJson") {
+                return new FormJsonModelBinder();
+            }
+
+            return null;
+        }
+    }
+}
