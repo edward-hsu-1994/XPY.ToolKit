82b5579e282e7555dfe460eaeac9dd1572f1d394
XuPeiYao <st89382000@gmail.com>
2019-09-07 22:49:46 +0800

    feat: 加入HTTP基礎驗證功能

---
+++ XPY.ToolKit.AspNetCore/IBaseAuthorizeHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace XPY.ToolKit.AspNetCore {
+    /// <summary>
+    /// 基礎驗證處理
+    /// </summary>
+    public interface IBaseAuthorizeHandler {
+        /// <summary>
+        /// 驗證方法
+        /// </summary>
+        /// <param name="account">帳號</param>
+        /// <param name="password">密碼</param>
+        /// <returns>驗證結果</returns>
+        Task<bool> Authorize(string account, string password);
+    }
+}
