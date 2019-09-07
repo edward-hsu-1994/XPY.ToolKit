3263824607f1f0487dce8d7b21215b25e5d11314
XuPeiYao <st89382000@gmail.com>
2019-09-07 12:41:04 +0800

    test: feat: 加入Services DI單元測試

---
+++ XPY.ToolKit.AspNetCore.Services.Test/WebProject/TestController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using XPY.ToolKit.AspNetCore.Services.Test.WebProject.Services;
+using Xunit;
+
+namespace XPY.ToolKit.AspNetCore.Mvc.Services.WebProject {
+    [Route("api/[Controller]")]
+    public class TestController : Controller {
+        [HttpGet]
+        public void Get(
+            [FromServices]TestService testService) {
+            Assert.NotNull(testService);
+        }
+    }
+}
