8b9a92b7b4655e806e6733e3f54c381ca619a3ba
XuPeiYao <st89382000@gmail.com>
2019-10-12 00:02:50 +0800

    feat: 加入RabbitMQ Queue Consumer實作與擴充方法

---
+++ XPY.ToolKit.AspNetCore.RabbitMQ/QueueConsumerBase.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XPY.ToolKit.AspNetCore.RabbitMQ
+{
+    /// <summary>
+    /// Rabbit Queue Consumer基礎類別
+    /// </summary>
+    public abstract class QueueConsumerBase
+    {
+        /// <summary>
+        /// RabbitMQ.Client套件的Consumer實例
+        /// </summary>
+        public EventingBasicConsumer Consumer { get; }
+
+        /// <summary>
+        /// Queue Consumer基礎類別建構子
+        /// </summary>
+        /// <param name="consumer">Consumer實例</param>
+        public QueueConsumerBase(
+            EventingBasicConsumer consumer)
+        {
+            Consumer = consumer;
+        }
+    }
+}
---
+++ XPY.ToolKit.AspNetCore.RabbitMQ/QueueConsumerOptions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XPY.ToolKit.AspNetCore.RabbitMQ
+{
+    /// <summary>
+    /// Rabbit Queue Consumer參數
+    /// </summary>
+    /// <typeparam name="TConsumer">Queue Consumer類型</typeparam>
+    public class QueueConsumerOptions<TConsumer>
+        where TConsumer : QueueConsumerBase
+    {
+        /// <summary>
+        /// Queue名稱
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 持續性
+        /// </summary>
+        public bool Durable { get; set; }
+
+        /// <summary>
+        /// 單一連線
+        /// </summary>
+        public bool Exclusive { get; set; }
+
+        /// <summary>
+        /// 自動刪除
+        /// </summary>
+        public bool AutoDelete { get; set; }
+
+        /// <summary>
+        /// 自動ACK
+        /// </summary>
+        public bool AutoAck { get; set; }
+        public IDictionary<string, object> Arguments { get; set; }
+    }
+}
